cca0fd99a45c359f5514990d040c7a44a8226828
hybfkuf <condryhebenstreit@gmail.com>
2025-11-07 01:53:20 +0800

    feat(module): add modules "twofa"

--- module/twofa/register.go
+++ module/twofa/register.go
@@ -5,7 +5,30 @@ import (
 	"github.com/forbearing/gst/types/consts"
 )
 
-// Register registers the models: TOTPBind
+// Register registers the models: TOTPBind, TOTPCheck, TOTPConfirm, TOTPDevice, TOTPStatus, TOTPUnbind and TOTPVerify.
+//
+// Modules, Payload and Result:
+//   - TOTPBind, TOTPBindRsp
+//   - TOTPCheck, TOTPCheckReq, TOTPCheckRsp
+//   - TOTPConfirm, TOTPConfirmReq, TOTPConfirmRsp
+//   - TOTPDevice
+//   - TOTPStatus, TOTPStatusRsp
+//   - TOTPUnbind, TOTPUnbindReq, TOTPUnbindRsp
+//   - TOTPVerify, TOTPVerifyReq, TOTPVerifyRsp
+//
+// Routes
+//   - POST     /api/2fa/totp/bind
+//   - POST     /api/2fa/totp/check
+//   - POST     /api/2fa/totp/confirm
+//   - POST     /api/2fa/totp/status
+//   - POST     /api/2fa/totp/unbind
+//   - POST     /api/2fa/totp/verify
+//   - POST     /api/2fa/totp/devices
+//   - DELETE   /api/2fa/totp/devices/:id
+//   - PUT      /api/2fa/totp/devices/:id
+//   - PATCH    /api/2fa/totp/devices/:id
+//   - GET      /api/2fa/totp/devices
+//   - GET      /api/2fa/totp/devices/:id
 func Register() {
 	module.Use[
 		*TOTPBind,
@@ -33,4 +56,44 @@ func Register() {
 		&TOTPConfirmModule{},
 		consts.PHASE_CREATE,
 	)
+
+	module.Use[
+		*TOTPDevice,
+		*TOTPDevice,
+		*TOTPDevice,
+		*TOTPDeviceService](
+		&TOTPDeviceModule{},
+		consts.PHASE_CREATE,
+		consts.PHASE_DELETE,
+		consts.PHASE_UPDATE,
+		consts.PHASE_PATCH,
+		consts.PHASE_LIST,
+		consts.PHASE_GET,
+	)
+
+	module.Use[
+		*TOTPStatus,
+		*TOTPStatus,
+		*TOTPStatusRsp,
+		*TOTPStatusService](
+		&TOTPStatusModule{},
+		consts.PHASE_LIST,
+	)
+
+	module.Use[
+		*TOTPUnbind,
+		*TOTPUnbindReq,
+		*TOTPUnbindRsp,
+		*TOTPUnbindService](
+		&TOTPUnbindModule{},
+		consts.PHASE_CREATE,
+	)
+
+	module.Use[
+		*TOTPVerify,
+		*TOTPVerifyReq,
+		*TOTPVerifyRsp,
+		*TOTPVerifyService](
+		&TOTPVerifyModule{},
+		consts.PHASE_CREATE)
 }
--- module/twofa/totp_device.go
+++ module/twofa/totp_device.go
@@ -5,8 +5,21 @@ import (
 
 	. "github.com/forbearing/gst/dsl"
 	"github.com/forbearing/gst/model"
+	"github.com/forbearing/gst/service"
+	"github.com/forbearing/gst/types"
 	"gorm.io/datatypes"
 )
+
+var _ types.Module[*TOTPDevice, *TOTPDevice, *TOTPDevice] = (*TOTPDeviceModule)(nil)
+
+type TOTPDeviceModule struct{}
+
+func (*TOTPDeviceModule) Service() types.Service[*TOTPDevice, *TOTPDevice, *TOTPDevice] {
+	return &TOTPDeviceService{}
+}
+func (*TOTPDeviceModule) Route() string { return "2fa/totp/devices" }
+func (*TOTPDeviceModule) Pub() bool     { return false }
+func (*TOTPDeviceModule) Param() string { return "id" }
 
 // TOTPDevice represents a TOTP device for 2FA
 type TOTPDevice struct {
@@ -24,20 +37,27 @@ func (TOTPDevice) Design() {
 	Migrate(true)
 
 	Route("2fa/totp/devices", func() {
+		Create(func() {
+			Enabled(true)
+		})
+		Delete(func() {
+			Enabled(true)
+		})
+		Update(func() {
+			Enabled(true)
+		})
+		Patch(func() {
+			Enabled(true)
+		})
 		List(func() {
 			Enabled(true)
 		})
 		Get(func() {
 			Enabled(true)
 		})
-		Create(func() {
-			Enabled(true)
-		})
-		Update(func() {
-			Enabled(true)
-		})
-		Delete(func() {
-			Enabled(true)
-		})
 	})
+}
+
+type TOTPDeviceService struct {
+	service.Base[*TOTPDevice, *TOTPDevice, *TOTPDevice]
 }
