9ed29339908adc0468b54d445c320deac7cced58
hybfkuf <condryhebenstreit@gmail.com>
2025-11-08 00:37:36 +0800

    feat(module): add "version" module

---
+++ module/version/register.go
@@ -0,0 +1,24 @@
+package version
+
+import (
+	"github.com/forbearing/gst/module"
+	"github.com/forbearing/gst/types/consts"
+)
+
+// Register registers the version module.
+//
+// Modals and Result:
+//   - Version, VersionRsp
+//
+// Routes:
+//   - GET /api/version
+func Register() {
+	module.Use[
+		*Version,
+		*Version,
+		*VersionRsp,
+		*VersionService](
+		&VersionModule{},
+		consts.PHASE_GET,
+	)
+}
