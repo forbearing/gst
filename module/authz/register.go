34e094ce87d906f899bf97d7854229767bca7469
hybfkuf <condryhebenstreit@gmail.com>
2025-11-08 17:53:41 +0800

    feat(module): add "Role" module

--- module/authz/register.go
+++ module/authz/register.go
@@ -16,4 +16,22 @@ func Register() {
 		consts.PHASE_LIST,
 		consts.PHASE_GET,
 	)
+
+	module.Use[
+		*Role,
+		*Role,
+		*Role,
+		*RoleService](
+		&RoleModule{},
+		consts.PHASE_CREATE,
+		consts.PHASE_DELETE,
+		consts.PHASE_UPDATE,
+		consts.PHASE_PATCH,
+		consts.PHASE_LIST,
+		consts.PHASE_GET,
+		consts.PHASE_CREATE_MANY,
+		consts.PHASE_DELETE_MANY,
+		consts.PHASE_UPDATE_MANY,
+		consts.PHASE_PATCH_MANY,
+	)
 }
---
+++ module/authz/role.go
@@ -0,0 +1,101 @@
+package authz
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/forbearing/gst/authz/rbac"
+	"github.com/forbearing/gst/database"
+	"github.com/forbearing/gst/model"
+	"github.com/forbearing/gst/service"
+	"github.com/forbearing/gst/types"
+	"github.com/forbearing/gst/types/consts"
+	"github.com/forbearing/gst/util"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type RoleModule struct{}
+
+func (*RoleModule) Service() types.Service[*Role, *Role, *Role] {
+	return &RoleService{}
+}
+func (*RoleModule) Route() string { return "roles" }
+func (*RoleModule) Pub() bool     { return false }
+func (*RoleModule) Param() string { return "id" }
+
+type Role struct {
+	Name string `json:"name,omitempty" schema:"name"`
+	Code string `json:"code,omitempty" schema:"code"`
+
+	model.Base
+}
+
+func (r *Role) CreateBefore(*types.ModelContext) error {
+	r.Name = strings.TrimSpace(r.Name)
+	r.Code = strings.TrimSpace(r.Code)
+	if len(r.Name) == 0 {
+		return errors.New("name is required")
+	}
+	if len(r.Code) == 0 {
+		return errors.New("code is required")
+	}
+	// Ensure the role with the same name/code share the same ID.
+	// If the role already exists, set same id to just update it.
+	r.SetID(util.HashID(r.Code))
+
+	return nil
+}
+func (r *Role) CreateAfter(*types.ModelContext) error      { return rbac.RBAC().AddRole(r.Name) }
+func (r *Role) UpdateBefore(ctx *types.ModelContext) error { return r.CreateAfter(ctx) }
+func (r *Role) DeleteBefore(ctx *types.ModelContext) error {
+	// The delete request always don't have role id, so we should get the role from database.
+	if err := database.Database[*Role](ctx.DatabaseContext()).Get(r, r.ID); err != nil {
+		return err
+	}
+	if len(r.Name) > 0 {
+		return rbac.RBAC().RemoveRole(r.Name)
+	}
+	return nil
+}
+
+func (r *Role) DeleteAfter(ctx *types.ModelContext) error {
+	return database.Database[*Role](ctx.DatabaseContext()).Cleanup()
+}
+
+func (r *Role) MarshalLogObject(enc zapcore.ObjectEncoder) error {
+	if r == nil {
+		return nil
+	}
+	enc.AddString("name", r.Name)
+	_ = enc.AddObject("base", &r.Base)
+	return nil
+}
+
+type RoleService struct {
+	service.Base[*Role, *Role, *Role]
+}
+
+// DeleteAfter support filter and delete multiple roles by query parameter `name`.
+func (r *RoleService) DeleteAfter(ctx *types.ServiceContext, role *Role) error {
+	log := r.WithServiceContext(ctx, consts.PHASE_DELETE_AFTER)
+	name := ctx.URL.Query().Get("name")
+	if len(name) == 0 {
+		return nil
+	}
+
+	roles := make([]*Role, 0)
+	if err := database.Database[*Role](ctx.DatabaseContext()).WithLimit(-1).WithQuery(&Role{Name: name}).List(&roles); err != nil {
+		log.Error(err)
+		return err
+	}
+	for _, role := range roles {
+		log.Infoz("will delete role", zap.Object("role", role))
+	}
+	if err := database.Database[*Role](ctx.DatabaseContext()).WithPurge().Delete(roles...); err != nil {
+		log.Error(err)
+		return err
+	}
+
+	return nil
+}
