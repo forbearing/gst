1294d45ac6d3a32a684ecc3ad250e40f1cfcfd2f
hybfkuf <condryhebenstreit@gmail.com>
2025-11-24 21:40:25 +0800

    refactor(module/authz): move API service and model to dedicated internal packages

--- module/authz/api.go
+++ module/authz/api.go
@@ -1,16 +1,19 @@
 package authz
 
 import (
-	"github.com/forbearing/gst/database"
-	"github.com/forbearing/gst/model"
-	"github.com/forbearing/gst/service"
+	modelauthz "github.com/forbearing/gst/internal/model/authz"
+	serviceauthz "github.com/forbearing/gst/internal/service/authz"
 	"github.com/forbearing/gst/types"
-	"github.com/samber/lo"
 )
 
 var _ types.Module[*API, *API, APIRsp] = (*APIModule)(nil)
 
-type APIModule struct{}
+type (
+	API        = modelauthz.API
+	APIRsp     = modelauthz.APIRsp
+	APIService = serviceauthz.APIService
+	APIModule  struct{}
+)
 
 func (*APIModule) Service() types.Service[*API, *API, APIRsp] {
 	return &APIService{}
@@ -18,30 +21,3 @@ func (*APIModule) Service() types.Service[*API, *API, APIRsp] {
 func (*APIModule) Route() string { return "/apis" }
 func (*APIModule) Pub() bool     { return false }
 func (*APIModule) Param() string { return "id" }
-
-type API struct{ model.Empty }
-
-type APIRsp = []string
-
-type APIService struct {
-	service.Base[*API, *API, APIRsp]
-}
-
-func (APIService) List(ctx *types.ServiceContext, req *API) (APIRsp, error) {
-	perms := make([]*Permission, 0)
-	if err := database.Database[*Permission](ctx.DatabaseContext()).List(&perms); err != nil {
-		return nil, err
-	}
-
-	apis := make([]string, 0)
-	for _, pem := range perms {
-		// api := strings.TrimSuffix(pem.Resource, "/{id}")
-		// api = strings.TrimSuffix(api, "/id")
-		// api = strings.TrimSuffix(api, "/batch")
-		// api = strings.TrimSuffix(api, "/")
-		// apis = append(apis, api)
-		apis = append(apis, pem.Resource)
-	}
-
-	return lo.Uniq(apis), nil
-}
