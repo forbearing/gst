7334aad1e9712119245362ae8f7048ebbc40af23
hybfkuf <condryhebenstreit@gmail.com>
2025-11-24 21:43:07 +0800

    refactor(module/authz): extrace ButtonService into internal/service layer

--- module/authz/button.go
+++ module/authz/button.go
@@ -2,13 +2,17 @@ package authz
 
 import (
 	modelauthz "github.com/forbearing/gst/internal/model/authz"
-	"github.com/forbearing/gst/service"
+	serviceauthz "github.com/forbearing/gst/internal/service/authz"
 	"github.com/forbearing/gst/types"
 )
 
 var _ types.Module[*Button, *Button, *Button] = (*ButtonModule)(nil)
 
-type ButtonModule struct{}
+type (
+	Button        = modelauthz.Button
+	ButtonService = serviceauthz.ButtonService
+	ButtonModule  struct{}
+)
 
 func (*ButtonModule) Service() types.Service[*Button, *Button, *Button] {
 	return &ButtonService{}
@@ -16,9 +20,3 @@ func (*ButtonModule) Service() types.Service[*Button, *Button, *Button] {
 func (*ButtonModule) Route() string { return "buttons" }
 func (*ButtonModule) Pub() bool     { return false }
 func (*ButtonModule) Param() string { return "id" }
-
-type Button = modelauthz.Button
-
-type ButtonService struct {
-	service.Base[*Button, *Button, *Button]
-}
